25381496e7c36a83288bcb5ee605844de201d923
Pranav Mishra <89926012+PranavMishra17@users.noreply.github.com>
2023-03-06 22:41:30 +0530

    added video player and pause/play

---
+++ Bipolar Townn TV/Assets/VideoplayerCtrl.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+using TMPro;
+
+public class VideoplayerCtrl : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public AudioSource audsource;
+    public GameObject playButton;
+    public float raycastDistance = 10f;
+    public TextMeshProUGUI txt;
+
+    private RaycastHit hit;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // Check if the left mouse button is clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Cast a ray from the camera's position in the direction of the mouse pointer
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // Check if the ray hits the play button
+            if (Physics.Raycast(ray, out hit, raycastDistance) && hit.collider.gameObject == playButton)
+            {
+                if (videoPlayer.isPlaying)
+                {
+                    videoPlayer.Pause();
+                    txt.text = "Play";
+                }
+                else  videoPlayer.Play();
+                txt.text = "Pause";
+            }
+        }
+    }
+
+
+}
